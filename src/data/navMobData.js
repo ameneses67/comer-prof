5b2d65af0458c459d02103fee6de853dc8dfc1ca
Poncho <ameneses67@gmail.com>
2023-02-05 19:02:32 -0600

    Mobile menu animation

---
+++ src/data/navMobData.js
@@ -0,0 +1,20 @@
+const mobNavData = [
+  {
+    name: "Inicio",
+    path: "#inicio",
+  },
+  {
+    name: "Nosotros",
+    path: "#nosotros",
+  },
+  {
+    name: "Productos",
+    path: "#productos",
+  },
+  {
+    name: "Clientes",
+    path: "#clientes",
+  },
+];
+
+export default mobNavData;
--- src/js/burgerAnimation.js
+++ src/js/burgerAnimation.js
@@ -3,10 +3,12 @@ const navmob = document.querySelector("#navmob");
 
 tham.addEventListener("click", () => {
   tham.classList.toggle("tham-active");
-  navmob.classList.toggle("hidden");
+  navmob.classList.toggle("scale-y-0");
+  navmob.classList.toggle("scale-y-100");
 });
 
 navmob.addEventListener("click", () => {
   tham.classList.toggle("tham-active");
-  navmob.classList.toggle("hidden");
+  navmob.classList.toggle("scale-y-0");
+  navmob.classList.toggle("scale-y-100");
 });
