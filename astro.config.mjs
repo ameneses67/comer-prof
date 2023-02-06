fd6dff759ef0ccc2566407ecc419ec4f5516fe08
Poncho <ameneses67@gmail.com>
2023-02-06 17:11:34 -0600

    GTM added via Partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,17 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    partytown({
+      forward: ["dataLayer.push"],
     }),
   ],
 });
